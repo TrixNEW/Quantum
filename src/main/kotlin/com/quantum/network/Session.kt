fd6ace4a811caf68009b2eb8867cf47a39def1fe
TrixNEW <cooldinothunder@gmail.com>
2026-02-16 00:42:23 -0500

    GameMode Switching
    Player Function to setGamemode.
    
    sendGameMode Function in Session that sends the gamemode swap packet.

--- src/main/kotlin/com/quantum/network/Session.kt
+++ src/main/kotlin/com/quantum/network/Session.kt
@@ -3,50 +3,52 @@ package com.quantum.network
 import com.quantum.Server
 import com.quantum.network.handler.ResourcePackPacketHandler
 import com.quantum.player.Player
+import com.quantum.player.model.GameMode
 import com.quantum.utils.Utils
 import org.cloudburstmc.protocol.bedrock.BedrockPeer
 import org.cloudburstmc.protocol.bedrock.BedrockServerSession
 import org.cloudburstmc.protocol.bedrock.netty.BedrockPacketWrapper
 import org.cloudburstmc.protocol.bedrock.packet.PlayStatusPacket
 import org.cloudburstmc.protocol.bedrock.packet.ResourcePacksInfoPacket
-import java.util.*
+import org.cloudburstmc.protocol.bedrock.packet.SetPlayerGameTypePacket
+import java.util.Objects
 
-/**
- * The client network session
- * @author xRookieFight
- * @since 03/02/2026
- */
 class Session(
-    peer: BedrockPeer,
-    subClientId: Int,
-    val server: Server
+	peer: BedrockPeer,
+	subClientId: Int,
+	val server: Server
 ) : BedrockServerSession(peer, subClientId) {
 
-    var player: Player? = null
+	var player: Player? = null
 
-    override fun onPacket(wrapper: BedrockPacketWrapper) {
-        Objects.requireNonNull(packetHandler, "Received a packet without handler")
-        packetHandler.handlePacket(wrapper.packet)
-    }
+	override fun onPacket(wrapper: BedrockPacketWrapper) {
+		Objects.requireNonNull(packetHandler, "Received a packet without handler")
+		packetHandler.handlePacket(wrapper.packet)
+	}
 
-    fun onLoginSuccess() {
-        val playStatusPacket = PlayStatusPacket().apply {
-            status = PlayStatusPacket.Status.LOGIN_SUCCESS
-        }
+	fun onLoginSuccess() {
+		val playStatusPacket = PlayStatusPacket().apply {
+			status = PlayStatusPacket.Status.LOGIN_SUCCESS
+		}
+		sendPacket(playStatusPacket)
 
-        sendPacket(playStatusPacket)
+		val resourcePacksInfoPacket = ResourcePacksInfoPacket().apply {
+			isForcedToAccept = false
+			isHasAddonPacks = false
+			isScriptingEnabled = false
+			worldTemplateId = Utils.EMPTY_UUID
+			worldTemplateVersion = ""
+			isVibrantVisualsForceDisabled = false
+		}
+		sendPacketImmediately(resourcePacksInfoPacket)
 
-        val resourcePacksInfoPacket = ResourcePacksInfoPacket().apply {
-            isForcedToAccept = false
-            isHasAddonPacks = false
-            isScriptingEnabled = false
-            worldTemplateId = Utils.EMPTY_UUID
-            worldTemplateVersion = ""
-            isVibrantVisualsForceDisabled = false
-        } // TODO: Resource pack system
+		packetHandler = ResourcePackPacketHandler().attach(this, server)
+	}
 
-        sendPacketImmediately(resourcePacksInfoPacket)
-
-        packetHandler = ResourcePackPacketHandler().attach(this, server)
-    }
+	fun sendGameMode(mode: GameMode) {
+		val packet = SetPlayerGameTypePacket().apply {
+			gamemode = mode.id
+		}
+		sendPacket(packet)
+	}
 }
--- src/main/kotlin/com/quantum/player/Player.kt
+++ src/main/kotlin/com/quantum/player/Player.kt
@@ -2,18 +2,32 @@ package com.quantum.player
 
 import com.quantum.Server
 import com.quantum.network.Session
-import java.util.*
+import com.quantum.player.model.GameMode
+import java.util.UUID
 
 class Player(
-    val uuid: UUID,
-    val name: String,
-    val xuid: String,
-    val session: Session
+	val uuid: UUID,
+	val name: String,
+	val xuid: String,
+	val session: Session,
+	gameMode: GameMode
 ) {
 
-    fun getServer() : Server
-    = session.server
+	var gameMode: GameMode = gameMode
+		private set
 
-    fun remove()
-    = getServer().playerManager.removePlayer(this)
+	fun getServer(): Server = session.server
+
+	fun setGameMode(mode: GameMode, sendUpdate: Boolean = true) {
+		if (mode == gameMode) return
+		gameMode = mode
+
+		// TODO: apply internal flags
+
+		if (sendUpdate) {
+			session.sendGameMode(mode)
+		}
+	}
+
+	fun remove() = getServer().playerManager.removePlayer(this)
 }
